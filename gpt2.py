82a70500593b753af1f9ad39ec70becd9c0bda7f
happybadending <152324383+happybadending@users.noreply.github.com>
2023-12-04 02:39:21 +0900

    force replace numpy with nn.py, make pretty later

--- gpt2.py
+++ gpt2.py
@@ -1,91 +1,100 @@
 # based on https://github.com/jaymody/picoGPT
-# TODO: move stuff into nn.py and remove numpy dependency
-# TODO: prettier load weights
 
+import nn
 import os, urllib.request
 import tiktoken
 import safetensors.numpy
-import numpy as np
 from tqdm import tqdm
 import fire
 
 class Embedding():
-  def __init__(self, weights):
-    self.weight = weights['weight']
-
+  def __init__(self): self.weight = None
   def __call__(self, x):
-    return self.weight[x]
+    assert isinstance(x, nn.Tensor), type(x)
+    assert not isinstance(x.data, nn.Tensor), type(x.data)
+    return self.weight[x.data]
 
 class Linear:
-  def __init__(self, weights):
-    self.weight = weights['weight']
-    self.bias = weights['bias']
-
+  def __init__(self): self.weight = self.bias = None
   def __call__(self, x):
+    assert isinstance(x, nn.Tensor), type(x)
+    assert not isinstance(x.data, nn.Tensor), type(x.data)
     return x @ self.weight + self.bias
 
 class Attention():
-  def __init__(self, weights):
-    self.c_attn = Linear(weights['c_attn'])
-    self.c_proj = Linear(weights['c_proj'])
+  def __init__(self):
+    self.bias = None
+    self.c_attn = Linear()
+    self.c_proj = Linear()
 
   def __call__(self, x, n_heads):
+    assert isinstance(x, nn.Tensor), type(x)
+    assert not isinstance(x.data, nn.Tensor), type(x.data)
     def softmax(x):
-      e = np.exp(x - np.max(x, axis=-1, keepdims=True))
-      return e / np.sum(e, axis=-1, keepdims=True)
+      e = nn.Tensor.exp((x - nn.Tensor.max(x.data, axis=-1, keepdims=True)).data)
+      return e / nn.Tensor.sum(e.data, axis=-1, keepdims=True)
     def attention(q, k, v, mask):
-      return softmax(q @ k.T / np.sqrt(q.shape[-1]) + mask) @ v
-    mask = (1 - np.tri(x.shape[0], dtype=x.dtype)) * -1e10
+      return softmax(q @ k.T / nn.Tensor.sqrt(q.shape[-1]) + mask) @ v
+    mask = (1 - nn.Tensor.tri(x.shape[0], dtype=x.data.dtype)) * -1e10
     y = self.c_attn(x)
-    qkv = np.split(y, 3, axis=-1) # TODO: add kv cache
-    qkv = list(map(lambda x: np.split(x, n_heads, axis=-1), qkv))
+    qkv = nn.Tensor.split(y.data, 3, axis=-1) # TODO: add kv cache
+    qkv = list(map(lambda x: nn.Tensor.split(x.data, n_heads, axis=-1), qkv.data))
     y = [attention(q, k, v, mask) for q, k, v in zip(*qkv)]
-    return self.c_proj(np.hstack(y))
+    for i in range(len(y)): y[i] = y[i].data
+    return self.c_proj(nn.Tensor.hstack(y))
 
 class FeedForward():
-  def __init__(self, weights):
-    self.c_fc = Linear(weights['c_fc'])
-    self.c_proj = Linear(weights['c_proj'])
+  def __init__(self):
+    self.c_fc = Linear()
+    self.c_proj = Linear()
 
   def __call__(self, x):
+    assert isinstance(x, nn.Tensor), type(x)
+    assert not isinstance(x.data, nn.Tensor), type(x.data)
     def gelu(x):
-      return 0.5 * x * (1 + np.tanh(x * 0.7978845608 * (1 + 0.044715 * x * x)))
+      return 0.5 * x * (1 + nn.Tensor.tanh(x.data * 0.7978845608 * (1 + 0.044715 * x.data * x.data)))
     return self.c_proj(gelu(self.c_fc(x)))
 
 class LayerNorm():
-  def __init__(self, weights):
-    self.weight = weights['weight']
-    self.bias = weights['bias']
+  def __init__(self):
+    self.weight = None
+    self.bias = None
 
   def __call__(self, x, eps=1e-5):
-    rms = ((x*x).mean(axis=-1, keepdims=True) + eps) ** 0.5
+    assert isinstance(x, nn.Tensor), type(x)
+    assert not isinstance(x.data, nn.Tensor), type(x.data)
+    rms = ((x*x).data.mean(axis=-1, keepdims=True) + eps) ** 0.5
     return x / rms * self.weight + self.bias
 
 class TransformerBlock():
-  def __init__(self, weights):
-    self.attn = Attention(weights['attn'])
-    self.mlp = FeedForward(weights['mlp'])
-    self.ln_1 = LayerNorm(weights['ln_1'])
-    self.ln_2 = LayerNorm(weights['ln_2'])
+  def __init__(self):
+    self.attn = Attention()
+    self.mlp = FeedForward()
+    self.ln_1 = LayerNorm()
+    self.ln_2 = LayerNorm()
 
   def __call__(self, x, n_heads):
+    assert isinstance(x, nn.Tensor), type(x)
+    assert not isinstance(x.data, nn.Tensor), type(x.data)
     y = x + self.attn(self.ln_1(x), n_heads)
     return y + self.mlp(self.ln_2(y))
 
 class Transformer():
-  def __init__(self, weights, params):
+  def __init__(self, params):
     self.p = params
-    self.wte = Embedding(weights['wte'])
-    self.wpe = Embedding(weights['wpe'])
-    self.h = [TransformerBlock(weights['block'][i]) for i in range(self.p['n_layers'])]
-    self.ln_f = LayerNorm(weights['ln_f'])
+    self.wte = Embedding()
+    self.wpe = Embedding()
+    self.block = [TransformerBlock() for _ in range(self.p['n_layers'])]
+    self.ln_f = LayerNorm()
 
   def __call__(self, x, temperature):
+    assert isinstance(x, nn.Tensor), type(x)
+    assert not isinstance(x.data, nn.Tensor), type(x.data)
     def softmax(x):
-      e = np.exp(x - np.max(x, axis=-1, keepdims=True))
-      return e / np.sum(e, axis=-1, keepdims=True)
-    y = self.wte(x) + self.wpe(range(len(x)))
-    for h in self.h: y = h(y, self.p['n_heads'])
+      e = nn.Tensor.exp((x - nn.Tensor.max(x.data, axis=-1, keepdims=True)).data)
+      return e / nn.Tensor.sum(e.data, axis=-1, keepdims=True)
+    y = self.wte(x) + self.wpe(nn.Tensor(range(len(x.data))))
+    for h in self.block: y = h(y, self.p['n_heads'])
     y = self.ln_f(y) @ self.wte.weight.T
     return softmax(y / (temperature + 1e-10))
 
@@ -94,18 +103,20 @@ class GPT2:
     self.model = model
     self.tokenizer = tokenizer
 
-  def generate(self, prompt, temperature=0, n_toks=50):
+  def generate(self, prompt='<|endoftext|>', n_toks=50, temperature=0):
     toks = self.tokenizer.encode(prompt)
-    for _ in tqdm(range(n_toks), 'cooking'):
-      logits = self.model(toks, temperature)
-      toks.append(int(np.argmax(logits[-1]))) # TODO: not be greedy
+    for _ in tqdm(range(n_toks)):
+      logits = self.model(nn.Tensor(toks), temperature)
+      toks.append(int(nn.Tensor.argmax(logits[-1].data).data)) # TODO: not be greedy
     print(self.tokenizer.decode(toks[len(toks) - n_toks:]))
 
   @staticmethod
   def build(model_size):
     params = {
       'gpt2': dict(n_layers=12, n_heads=12, dim=768, n_vocab=50257, eps=1e-5),
-      # TODO: maybe add more sizes
+      'gpt2-medium': dict(n_layers=24, n_heads=16, dim=1024, n_vocab=50257, eps=1e-5),
+      'gpt2-large': dict(n_layers=36, n_heads=20, dim=1280, n_vocab=50257, eps=1e-5),
+      'gpt2-xl': dict(n_layers=48, n_heads=25, dim=1600, n_vocab=50257, eps=1e-5),
     }[model_size]
     if not os.path.exists(model_size+'.safetensors'): urllib.request.urlretrieve('https://huggingface.co/'+model_size+'/resolve/main/model.safetensors', model_size+'.safetensors')
     weights = dict(block=[{} for _ in range(params['n_layers'])])
@@ -118,7 +129,18 @@ class GPT2:
       p = k.split('.')
       if k.startswith('h'): insert(weights['block'][int(p[1])], p[2:], v)
       else: insert(weights, p, v)
-    return GPT2(Transformer(weights, params), tiktoken.get_encoding(model_size))
+    model = GPT2(Transformer(params), tiktoken.get_encoding(model_size))
+    def load_weights(cls, w):
+      for k,v in w.items():
+        if isinstance(v, dict):
+          load_weights(getattr(cls, k), v)
+        elif isinstance(v, list):
+          for i,d in enumerate(v):
+            load_weights(getattr(cls, k)[i], d)
+        else:
+          setattr(cls, k, nn.Tensor(v))
+    load_weights(model.model, weights)
+    return model
 
 gpt = GPT2.build('gpt2')
 fire.Fire(gpt.generate)
--- nn.py
+++ nn.py
@@ -6,17 +6,46 @@ class Tensor:
     self.grad = None
     self.ctx = ctx
 
-  def __repr__(self):
-    return f"<Tensor {self.data} grad {self.grad}>"
+  def __repr__(self): return f"<Tensor {self.data} grad {self.grad}>"
+  def __getitem__(self, x): return Tensor(self.data[x])
 
+  @property
+  def shape(self): return self.data.shape
+  @property
+  def T(self): return Tensor(self.data.T)
+
+  @staticmethod
+  def tensor(x): return Tensor(x) if not isinstance(x, Tensor) else x
   def __neg__(self): return Neg.apply(self)
+  def __add__(self, x): return Add.apply(self, self.tensor(x))
+  def __sub__(self, x): return Sub.apply(self, self.tensor(x))
+  def __mul__(self, x): return Mul.apply(self, self.tensor(x))
+  def __pow__(self, x): return Pow.apply(self, self.tensor(x))
+  def __truediv__(self, x): return Div.apply(self, self.tensor(x))
+  def __matmul__(self, x): return MatMul.apply(self, self.tensor(x))
 
-  def __add__(self, x): return Add.apply(self, x)
-  def __sub__(self, x): return Sub.apply(self, x)
-  def __mul__(self, x): return Mul.apply(self, x)
-  def __pow__(self, x): return Pow.apply(self, x)
-  def __truediv__(self, x): return Div.apply(self, x)
-  def __matmul__(self, x): return MatMul.apply(self, x)
+  def __radd__(self, x): return Add.apply(self.tensor(x), self)
+  def __rsub__(self, x): return Sub.apply(self.tensor(x), self)
+  def __rmul__(self, x): return Mul.apply(self.tensor(x), self)
+
+  @staticmethod
+  def argmax(*args, **kwargs): return Tensor(np.argmax(*args, **kwargs))
+  @staticmethod
+  def tanh(*args, **kwargs): return Tensor(np.tanh(*args, **kwargs))
+  @staticmethod
+  def tri(*args, **kwargs): return Tensor(np.tri(*args, **kwargs))
+  @staticmethod
+  def split(*args, **kwargs): return Tensor(np.split(*args, **kwargs))
+  @staticmethod
+  def sqrt(*args, **kwargs): return Tensor(np.sqrt(*args))
+  @staticmethod
+  def exp(*args, **kwargs): return Tensor(np.exp(*args, **kwargs))
+  @staticmethod
+  def max(*args, **kwargs): return Tensor(np.max(*args, **kwargs))
+  @staticmethod
+  def sum(*args, **kwargs): return Tensor(np.sum(*args, **kwargs))
+  @staticmethod
+  def hstack(*args, **kwargs): return Tensor(np.hstack(*args, **kwargs))
 
   def backward(self):
     def toposort(node, visited, ret):
